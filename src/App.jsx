c7677b56cbc6c202721294812b6b10036608b189
vinsonw <me@vinsonwei.com>
2024-04-05 21:02:44 +0800

    setup for 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import React from "../core/React.js"
 
-let showBar = false
-const Bar = <div id={"bar"}>bar</div>
-const Foo = (
-  <div id={"foo"}>
-    foo
-    <div>foo child 1</div>
-    <div>foo child 2</div>
-  </div>
-)
-
-function Counter() {
-  const handleShowBar = () => {
-    showBar = !showBar
+let countFoo = 1
+const Foo = () => {
+  console.log("Foo runs")
+  const handleClick = () => {
+    countFoo++
     React.update()
   }
-
   return (
     <div>
-      <button onClick={handleShowBar}>toggle</button>
-      {showBar && Bar}
-      <div>counter bottom</div>
+      <h2>Foo</h2>
+      {countFoo}
+      <button onClick={handleClick}>inc bar count</button>
+    </div>
+  )
+}
+
+let countBar = 1
+const Bar = () => {
+  console.log("Bar runs")
+  const handleClick = () => {
+    countBar++
+    React.update()
+  }
+  return (
+    <div>
+      <h2>bar</h2>
+      {countBar}
+      <button onClick={handleClick}>inc bar count</button>
     </div>
   )
 }
@@ -28,7 +35,8 @@ function Counter() {
 const App = () => (
   <div id="app">
     app mini-react
-    <Counter />
+    <Foo />
+    <Bar />
   </div>
 )
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from "../core/ReactDOM.js"
 import React from "../core/React.js"
 import App from "./App.jsx"
 
-console.log("App", App)
+console.log("main.jsx: App", App)
 
 ReactDOM.createRoot(document.querySelector("#root")).render(<App />)
